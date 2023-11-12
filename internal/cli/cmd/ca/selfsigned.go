04055a7453089455815c3edb26ec2da17e24b7d4
Zsolt Varga <zsolt.varga@console.hu>
2023-11-12 21:28:08 +0100

    self signed ca gen command and support reading ca pem from file

---
+++ internal/cli/cmd/ca/selfsigned.go
@@ -0,0 +1,64 @@
+/*
+ * The MIT License (MIT)
+ * Copyright (c) 2023 Cisco and/or its affiliates. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package ca
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+
+	"github.com/cisco-open/nasp/internal/cli"
+	"github.com/cisco-open/nasp/pkg/tls"
+)
+
+type caSelfSignedCommand struct {
+	cli cli.CLI
+}
+
+func NewSelfSignedCommand(c cli.CLI) *cobra.Command {
+	command := &caSelfSignedCommand{
+		cli: c,
+	}
+
+	cmd := &cobra.Command{
+		Use:               "generate-self-signed",
+		Aliases:           []string{"gen"},
+		Short:             "Generate and dump self signed CA",
+		SilenceErrors:     true,
+		SilenceUsage:      true,
+		DisableAutoGenTag: true,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return command.run(cmd)
+		},
+	}
+
+	return cmd
+}
+
+func (c *caSelfSignedCommand) run(cmd *cobra.Command) error {
+	caSigner, err := tls.NewSignerCA("")
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("%s%s", caSigner.Certificate.GetPEM(), caSigner.PrivateKey.GetPEM())
+
+	return nil
+}
