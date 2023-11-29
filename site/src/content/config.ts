942a7d6463bffbec4131a5ed5bc85ce1fa449cbe
Balazs Berta <balazs.berta.dev@gmail.com>
2023-11-29 13:40:48 +0100

    Small cleanup

--- site/src/content/config.ts
+++ site/src/content/config.ts
@@ -1,6 +1,6 @@
 // 1. Import utilities from `astro:content`
-import { z, defineCollection } from 'astro:content';
-import { SITE } from '../consts'
+import { z, defineCollection } from "astro:content";
+import { SITE } from "../consts";
 
 // 2. Define your collection(s)
 const blogCollection = defineCollection({
@@ -12,45 +12,32 @@ const blogCollection = defineCollection({
       src: z.string(),
       alt: z.string(),
     }),
-    publishDate: z.string().transform(str => new Date(str)),
-    author: z.string().default('Astroship'),
+    publishDate: z.string().transform((str) => new Date(str)),
+    author: z.string().default("Nasp"),
     category: z.string(),
     tags: z.array(z.string()),
   }),
 });
 
-const teamCollection = defineCollection({
-  schema: z.object({
-    draft: z.boolean(),
-    name: z.string(),
-    title: z.string(),
-    avatar: z.object({
-      src: z.string(),
-      alt: z.string(),
-    }),
-    publishDate: z.string().transform(str => new Date(str)),
-  }),
-});
 
 const docsCollection = defineCollection({
   schema: z.object({
     title: z.string().default(SITE.title),
     description: z.string().default(SITE.description),
-    dir: z.union([z.literal('ltr'), z.literal('rtl')]).default('ltr'),
+    dir: z.union([z.literal("ltr"), z.literal("rtl")]).default("ltr"),
     image: z
       .object({
         src: z.string(),
-        alt: z.string()
+        alt: z.string(),
       })
       .optional(),
-    ogLocale: z.string().optional()
-  })
-})
+    ogLocale: z.string().optional(),
+  }),
+});
 
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
-  'blog': blogCollection,
-  'team': teamCollection,
-  'docs': docsCollection,
+  blog: blogCollection,
+  docs: docsCollection,
 };
