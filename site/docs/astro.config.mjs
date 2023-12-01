159843e6e4b74cd8f73d1adfc0266170e4ab81fb
Zsolt Varga <zsolt.varga@console.hu>
2023-12-01 16:01:18 +0100

    add a separate docs site

---
+++ site/docs/astro.config.mjs
@@ -0,0 +1,38 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	base: "/docs",
+	themes: ["aa"],
+	darkMode: false,
+	integrations: [
+		starlight({
+			title: 'Nasp documentation',
+			logo: {
+				dark: './src/assets/logo-white.svg',
+				light: './src/assets/logo.svg',
+			},
+			social: {
+				github: 'https://github.com/cisco-open/nasp',
+			},
+			sidebar: [
+				{
+					label: 'Overview',
+					autogenerate: { directory: 'overview' },
+				},
+				{
+					label: 'Getting started',
+					autogenerate: { directory: 'start' },
+				},
+			],
+			customCss: [
+				'/src/styles/custom.css',
+			],
+			components: {
+				// Override the default `SocialIcons` component.
+				ThemeSelect: './src/components/ThemeSelect.astro',
+			},
+		}),
+	],
+});
