fc6a4fda00b0f667561b592520d4be524bd5868f
Zsolt Varga <zsolt.varga@console.hu>
2023-12-04 11:26:49 +0100

    site: remove blog from navbar

--- site/docs/astro.config.mjs
+++ site/docs/astro.config.mjs
@@ -4,7 +4,6 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	base: "/docs",
-	themes: ["aa"],
 	darkMode: false,
 	integrations: [
 		starlight({
