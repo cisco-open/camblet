9435cac1db1717c8cf3c50439ea3131001ce327c
Zsolt Varga <zsolt.varga@console.hu>
2023-12-11 12:04:00 +0100

    site: add google analytics

--- site/landing/astro.config.mjs
+++ site/landing/astro.config.mjs
@@ -1,22 +1,24 @@
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 import alpinejs from "@astrojs/alpinejs";
-
-// https://astro.build/config
 import image from "@astrojs/image";
-
-// https://astro.build/config
 import mdx from "@astrojs/mdx";
+import partytown from '@astrojs/partytown';
 
-// https://astro.build/config
 export default defineConfig({
   site: "http://localhost:8080/",
   base: "/",
-  integrations: [alpinejs(), tailwind(), image({
-    serviceEntryPoint: "@astrojs/image/sharp"
-  }), mdx()]
+  integrations: [
+    alpinejs(),
+    tailwind(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp"
+    }),
+    mdx(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ]
 });
