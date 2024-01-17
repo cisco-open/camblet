4a975f10a0ce7941741979ecdce5ccf4ef60c632
Zsolt Varga <zsolt.varga@console.hu>
2024-01-17 19:50:05 +0100

    refactor to use grpc api types

---
+++ internal/policy/policy.go
@@ -0,0 +1,90 @@
+/*
+ * The MIT License (MIT)
+ * Copyright (c) 2023 Cisco and/or its affiliates. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package policy
+
+import (
+	"context"
+	"reflect"
+	"sort"
+
+	"github.com/cisco-open/nasp/api/v1/core"
+)
+
+type RawPolicy = core.Policy
+type RawPolicies []*RawPolicy
+
+type HandlerFunc func(Policies)
+
+type Loader interface {
+	Run(context.Context, HandlerFunc) error
+}
+
+type Policies []*Policy
+
+type Selectors []map[string]bool
+
+type Policy struct {
+	Position int    `json:"position"`
+	ID       string `json:"id,omitempty"`
+
+	Selectors   Selectors                `json:"selectors,omitempty"`
+	Certificate *core.Policy_Certificate `json:"certificate,omitempty"`
+	Connection  *core.Policy_Connection  `json:"connection,omitempty"`
+	Egress      Policies                 `json:"egress,omitempty"`
+}
+
+func (r Policies) Organize() {
+	sort.SliceStable(r, func(i, j int) bool {
+		for _, sel := range r[i].Selectors {
+			for _, sel2 := range r[j].Selectors {
+				if reflect.DeepEqual(sel, sel2) {
+					continue
+				}
+
+				if IsMapSubset(sel, sel2) {
+					return true
+				}
+			}
+		}
+
+		return false
+	})
+
+	for k, rule := range r {
+		rule := rule
+
+		rule.Position = k
+		rule.Egress.Organize()
+	}
+}
+
+func IsMapSubset[K, V comparable](m, sub map[K]V) bool {
+	if len(sub) > len(m) {
+		return false
+	}
+
+	for k, vsub := range sub {
+		if vm, found := m[k]; !found || vm != vsub {
+			return false
+		}
+	}
+
+	return true
+}
---
+++ internal/service/convert.go
@@ -0,0 +1,158 @@
+/*
+ * The MIT License (MIT)
+ * Copyright (c) 2023 Cisco and/or its affiliates. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package service
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"net/netip"
+	"strconv"
+	"strings"
+
+	"github.com/cisco-open/nasp/api/v1/core"
+)
+
+func ConvertEntries(rawServices RawServices, withTags map[string]struct{}) Services {
+	entries := Services{}
+
+	filterAddressByTag := func(address *core.Service_Address) bool {
+		if len(address.GetTags()) == 0 || len(withTags) == 0 {
+			return true
+		}
+
+		var found bool
+		for _, tag := range address.GetTags() {
+			if _, ok := withTags[tag]; ok {
+				found = ok
+			}
+		}
+
+		return found
+	}
+
+	for _, svc := range rawServices {
+		// skip invalid entries
+		if len(svc.Labels) == 0 || len(svc.Addresses) == 0 {
+			continue
+		}
+		for _, address := range svc.Addresses {
+			if !filterAddressByTag(address) {
+				continue
+			}
+			addrs := resolveAddress(address.GetAddress() + ":" + strconv.Itoa(int(address.GetPort())))
+			for _, addr := range addrs {
+				MergeEntries(entries, Services{
+					addressToString(addr): &Service{
+						Labels: flattenLabels(svc.Labels),
+					},
+				})
+			}
+		}
+	}
+
+	return entries
+}
+
+func MergeEntries(l, r Services) Services {
+	for k, e := range r {
+		if _, ok := l[k]; !ok {
+			l[k] = e
+		} else {
+			l[k].Labels = append(l[k].Labels, e.Labels...)
+		}
+	}
+
+	return l
+}
+
+func flattenLabels(labels map[string]string) []string {
+	flattened := make([]string, 0)
+
+	for k, v := range labels {
+		flattened = append(flattened, k+":"+v)
+	}
+
+	return flattened
+}
+
+func addressToString(addr netip.AddrPort) string {
+	if addr.Port() == 0 {
+		return addr.Addr().StringExpanded()
+	}
+
+	return fmt.Sprintf("%s:%d", addr.Addr().StringExpanded(), addr.Port())
+}
+
+func resolveAddress(address string) []netip.AddrPort {
+	hasPort := strings.ContainsRune(address, ':')
+	addrporrs := make([]netip.AddrPort, 0)
+
+	var host, port string
+	var err error
+
+	if hasPort {
+		host, port, err = net.SplitHostPort(address)
+		if err != nil {
+			return nil
+		}
+	} else {
+		host = address
+	}
+
+	var intport int
+	if port != "" {
+		intport, err = strconv.Atoi(port)
+		if err != nil {
+			return nil
+		}
+	}
+
+	// there are some issues at some systems retrieving ipv4 address when there are ipv6 addresses as well
+	// for that reason there is a separate direct ipv4 lookup
+	// TODO @wayne - more investigation needed
+	addrs := make(map[netip.Addr]struct{})
+	if addr, err := netip.ParseAddr(host); err != nil {
+		netips, err := net.DefaultResolver.LookupIP(context.Background(), "ip4", host)
+		if err == nil {
+			for _, ip := range netips {
+				if addr, ok := netip.AddrFromSlice(ip.To4()); ok {
+					addrs[addr] = struct{}{}
+				}
+			}
+		}
+		netips, err = net.DefaultResolver.LookupIP(context.Background(), "ip", host)
+		if err == nil {
+			for _, ip := range netips {
+				if addr, ok := netip.AddrFromSlice(ip); ok {
+					addrs[addr] = struct{}{}
+				}
+			}
+		}
+	} else {
+		addrs[addr] = struct{}{}
+	}
+
+	for addr := range addrs {
+		addrporrs = append(addrporrs, netip.AddrPortFrom(addr, uint16(intport)))
+	}
+
+	return addrporrs
+}
