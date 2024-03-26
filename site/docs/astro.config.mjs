da48e7e0e1e25c264093d737387046ecf30b6155
Balazs Berta <14862689+hi-im-aren@users.noreply.github.com>
2024-03-26 15:17:25 +0100

    Remove banner, other fixes (#131)

--- site/docs/astro.config.mjs
+++ site/docs/astro.config.mjs
@@ -2,93 +2,95 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import partytown from '@astrojs/partytown';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-	base: "/docs",
-	darkMode: false,
-	integrations: [
-		partytown({
-			config: {
-				forward: ["dataLayer.push"],
-			},
-		}),
-		starlight({
-			title: 'Camblet documentation',
-			logo: {
-				dark: './src/assets/newlogo.png',
-				light: './src/assets/newlogo.png',
-			},
-			social: {
-				github: 'https://github.com/cisco-open/camblet',
-			},
-			sidebar: [
-				{
-					label: 'Get Started',
-					autogenerate: { directory: 'start' },
-				},
-				{
-					label: 'Concepts',
-					autogenerate: { directory: 'concepts' },
-				},
-				{
-					label: 'Talks',
-					link: '/talks/',
-					attrs: { style: 'font-weight: normal' },
-				},
-				{
-					label: 'Support',
-					link: '/support/',
-					attrs: { style: 'font-weight: normal' },
-				},
-				{
-					label: 'Contributing guide',
-					link: '/contributing/',
-					attrs: { style: 'font-weight: normal' },
-				},
-				{
-					label: 'Security procedures',
-					link: '/security/',
-					attrs: { style: 'font-weight: normal' },
-				},
-				{
-					label: 'Code of Conduct',
-					link: '/code-of-conduct/',
-					attrs: { style: 'font-weight: normal' },
-				},
-			],
-			customCss: [
-				'/src/styles/custom.css',
-			],
-			components: {
-				ThemeSelect: './src/components/ThemeSelect.astro',
-			},
-			expressiveCode: {
-				themes: ['dracula-soft'],
-				useStarlightDarkModeSwitch: false,
-				defaultProps: {
-					wrap: true,
-					overridesByLang: {
-						'bash,sh,zsh': { wrap: true }
-					}
-				},
-			},
-			head: [
-				{
-					tag: 'script',
-					attrs: {
-						type: 'text/partytown',
-						async: true,
-						src: 'https://www.googletagmanager.com/gtag/js?id=G-K315W0LM1X'
-					},
-				},
-				{
-					tag: 'script',
-					attrs: {
-						type: 'text/partytown'
-					},
-					content: 'window.dataLayer = window.dataLayer || []; function gtag() { dataLayer.push(arguments); } gtag("js", new Date()); gtag("config", "G-K315W0LM1X");'
-				},
-			],
-		}),
-	],
+  base: "/docs",
+  darkMode: false,
+  integrations: [partytown({
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  }), starlight({
+    title: 'Camblet documentation',
+    logo: {
+      dark: './src/assets/logo.svg',
+      light: './src/assets/logo.svg'
+    },
+    social: {
+      github: 'https://github.com/cisco-open/camblet'
+    },
+    sidebar: [{
+      label: 'Get Started',
+      autogenerate: {
+        directory: 'start'
+      }
+    }, {
+      label: 'Concepts',
+      autogenerate: {
+        directory: 'concepts'
+      }
+    }, {
+      label: 'Talks',
+      link: '/talks/',
+      attrs: {
+        style: 'font-weight: normal'
+      }
+    }, {
+      label: 'Support',
+      link: '/support/',
+      attrs: {
+        style: 'font-weight: normal'
+      }
+    }, {
+      label: 'Contributing guide',
+      link: '/contributing/',
+      attrs: {
+        style: 'font-weight: normal'
+      }
+    }, {
+      label: 'Security procedures',
+      link: '/security/',
+      attrs: {
+        style: 'font-weight: normal'
+      }
+    }, {
+      label: 'Code of Conduct',
+      link: '/code-of-conduct/',
+      attrs: {
+        style: 'font-weight: normal'
+      }
+    }],
+    customCss: ['/src/styles/custom.css'],
+    components: {
+      ThemeSelect: './src/components/ThemeSelect.astro'
+    },
+    expressiveCode: {
+      themes: ['dracula-soft'],
+      useStarlightDarkModeSwitch: false,
+      defaultProps: {
+        wrap: true,
+        overridesByLang: {
+          'bash,sh,zsh': {
+            wrap: true
+          }
+        }
+      }
+    },
+    head: [{
+      tag: 'script',
+      attrs: {
+        type: 'text/partytown',
+        async: true,
+        src: 'https://www.googletagmanager.com/gtag/js?id=G-K315W0LM1X'
+      }
+    }, {
+      tag: 'script',
+      attrs: {
+        type: 'text/partytown'
+      },
+      content: 'window.dataLayer = window.dataLayer || []; function gtag() { dataLayer.push(arguments); } gtag("js", new Date()); gtag("config", "G-K315W0LM1X");'
+    }]
+  }), tailwind()]
 });
