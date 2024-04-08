6e05b8af7bfaa60fab4b19da479cb30a267324f1
Nándor István Krácser <bonifaido@gmail.com>
2024-04-08 14:30:46 +0200

    fix ipv6 loopback resolving

--- internal/service/convert.go
+++ internal/service/convert.go
@@ -142,6 +142,10 @@ func resolveAddress(address string) []netip.AddrPort {
 		if err == nil {
 			for _, ip := range netips {
 				if addr, ok := netip.AddrFromSlice(ip); ok {
+					// if the address is ipv4 in ipv6 format and it is a loopback address, convert it to ipv6 loopback
+					if addr.Is4In6() && addr.IsLoopback() {
+						addr = netip.IPv6Loopback()
+					}
 					addrs[addr] = struct{}{}
 				}
 			}
